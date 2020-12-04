81e1bc7a137fc4f10c0849baef281f845b7b4d5e
Pfliger <pfliger@list.ru>
2020-12-04 09:05:34 +0300

    init commit

---
+++ application/db/people.py
@@ -0,0 +1,5 @@
+from datetime import datetime as dt
+
+
+def get_employees():
+    print(str(dt.now().date()))
---
+++ application/salary.py
@@ -0,0 +1,5 @@
+from datetime import datetime as dt
+
+
+def calculate_salary():
+    print(str(dt.now().date()))
